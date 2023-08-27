19303c244009f03c60a0ea2ca94f56cc2d46210a
edden <eddem.ardsantos@aluno.uepa.br>
2023-08-27 16:05:14 -0300

    menu principal adicionado

--- User Interface  (UI)/mainMenu.gd
+++ User Interface  (UI)/mainMenu.gd
@@ -3,3 +3,7 @@ extends CanvasLayer
 func _on_start_pressed():
 	print("botão pressionado")
 	get_tree().change_scene_to_file("res://Player/player.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
